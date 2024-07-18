ebbfc1d66c44a7d9e922f71d7aaf49b9e26cd469
ccmacas <celiamacas24@gmail.com>
2024-07-18 02:13:12 -0500

    cambio en el servicio Uniformes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const BASE_URL = 'http://192.168.3.5:3000/ApiMinig';
 
-export const MATERIAL_URL = "http://ec2-18-221-251-92.us-east-2.compute.amazonaws.com/ApiMinig";
-export const UNIFORM_URL = "http://ec2-18-221-189-23.us-east-2.compute.amazonaws.com/ApiMinig";
-export const AUTH_URL = "http://ec2-3-144-100-134.us-east-2.compute.amazonaws.com/ApiMinig";
+export const MATERIAL_URL = "http://ec2-3-131-85-164.us-east-2.compute.amazonaws.com/ApiMinig";
+export const UNIFORM_URL = "http://ec2-3-138-110-121.us-east-2.compute.amazonaws.com/ApiMinig";
+export const AUTH_URL = "http://ec2-3-146-107-87.us-east-2.compute.amazonaws.com/ApiMinig";
